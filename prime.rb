c46dfa237cc1b9c687342d329b08b2e49563b03d
Gil Sunshine <gilsunshine@gmail.com>
2018-01-09 18:18:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  is_prime = true
+  to_number = Array (2..(number - 1))
+  if number < 2
+    is_prime = false
+    break
+  end
+  to_number.each do |item|
+    if number % item == 0
+      is_prime = false
+      break
+    else
+    end
+  end
+  is_prime
+end
