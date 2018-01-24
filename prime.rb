1815b00eb9c415f5b6beb9b473190ab8ec96f3d8
danl411412 <daan.b.lee@gmail.com>
2018-01-24 05:07:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  range = 2...num
+  is_it_prime? = true
+  range.each do |x|
+    if num % x == 0
+      is_it_prime? = false
+    else
+      is_it_prime? = true
+  end
+  is_it_prime?
+end
